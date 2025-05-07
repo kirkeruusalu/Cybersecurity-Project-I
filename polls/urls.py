986347c5cefe4ea61d0708a3e4c18dac4ebdd4b1
Kirke Ruusalu <kirke.ruusalu@gmail.com>
2025-05-07 20:59:00 +0300

    login and register

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.beginning, name="beginning"),
+    path("index/", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("register/", views.register, name="register"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,9 @@ from django.db.models import F
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
 from .models import Choice, Question
 
@@ -22,6 +24,15 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+class CustomLoginView(LoginView):
+    template_name = "polls/registration/login.html"
+
+class CustomLogoutView(LogoutView):
+    template_name = "polls/registration/login.html"
+
+def beginning(request):
+    return render(request, "polls/beginning.html")
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -39,3 +50,13 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("polls:login")
+    else:
+        form = UserCreationForm()
+    return render(request, "polls/registration/register.html", {"form": form})
